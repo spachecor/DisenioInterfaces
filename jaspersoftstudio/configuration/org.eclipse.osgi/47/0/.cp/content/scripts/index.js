9b5b60ba75bfb60387830c9d6237bd20f594701a
Selene <spachecor03@gmail.com>
2025-01-29 14:03:39 +0100

    maniana 29012025

---
+++ jaspersoftstudio/configuration/org.eclipse.osgi/47/0/.cp/content/scripts/index.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2018 - 2019  Cloud Software Group, Inc. All rights reserved.
+ * 
+ * -----
+ * File Created: Wednesday, 18th September 2019 10:32:37 am
+ * Last Modified: Tuesday, 24th September 2019 11:51:41 am
+ * Modified By: gtoffoli (gtoffoli@tibco.com>)
+ * -----
+ * Author: gtoffoli (gtoffoli@tibco.com)
+ */
+
+		$(document).ready(function(){
+				
+                setTimeout( function() {
+                    $("#title").addClass("enter");
+                    $(".widget").addClass("enter");
+                }, 250);
+        
+        });
