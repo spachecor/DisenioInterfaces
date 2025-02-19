14f353c35236ec32aeea5f43b880e22f8d6cb2f9
Selene <spachecor03@gmail.com>
2025-02-19 09:41:15 +0100

    avance interfaz grafica

--- JavaFX/pacheco_selene_TF_DI/src/main/java/com/spachecor/pacheco_selene_tf_di/controller/PedidosController.java
+++ JavaFX/pacheco_selene_TF_DI/src/main/java/com/spachecor/pacheco_selene_tf_di/controller/PedidosController.java
@@ -1,4 +1,12 @@
 package com.spachecor.pacheco_selene_tf_di.controller;
 
+import com.spachecor.pacheco_selene_tf_di.controller.service.FXService;
+import javafx.fxml.FXML;
+
 public class PedidosController {
+
+    @FXML
+    private void onClickSalirButton(){
+        FXService.cambiarVentana(FXService.MAIN_VIEW);
+    }
 }
